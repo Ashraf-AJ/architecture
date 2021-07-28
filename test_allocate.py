af43ef3916fc91b6819c55a88e58754a70657697
Ashraf-AJ <ashraf.aj7@gmail.com>
2021-07-28 06:20:00 -0500

    fix some arrangement sections in test_allocate.py

--- test_allocate.py
+++ test_allocate.py
@@ -10,20 +10,20 @@ later = tomorrow + timedelta(days=10)
 def test_prefers_current_stock_batches_to_shipments():
     sku = "RETRO-CLOCK"
     in_stock_batch = Batch("in-stock-batch", sku, 100, eta=None)
-    shipment_batch = Batch("shipment-batch", sku, 100, eta=None)
+    shipment_batch = Batch("shipment-batch", sku, 100, eta=tomorrow)
     line = OrderLine("oref", sku, 10)
-    allocate(line, [in_stock_batch, shipment_batch])
+    allocate(line, [shipment_batch, in_stock_batch])
     assert in_stock_batch.available_quantity == 90
     assert shipment_batch.available_quantity == 100
 
 
 def test_prefers_earlier_batches():
     sku = "MINIMALIST-SPOON"
-    earliest = Batch("speedy-batch", sku, 100, eta=None)
-    medium = Batch("normal-batch", sku, 100, eta=None)
-    latest = Batch("slow-batch", sku, 100, eta=None)
+    earliest = Batch("speedy-batch", sku, 100, eta=today)
+    medium = Batch("normal-batch", sku, 100, eta=tomorrow)
+    latest = Batch("slow-batch", sku, 100, eta=later)
     line = OrderLine("oref", sku, 10)
-    allocate(line, [earliest, medium, latest])
+    allocate(line, [medium, earliest, latest])
     assert earliest.available_quantity == 90
     assert medium.available_quantity == 100
     assert latest.available_quantity == 100
